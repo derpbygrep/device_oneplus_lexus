4225ed0dd28cd32a1b1839f24f520a4c63f68968
inferno0230 <mail@inferno0230.in>
2025-03-12 18:41:44 +0000

    lexus: SensorPropsShim: Hardcode udfps location
    Oplus fingerprint HAL is setting out-of-bounds FOD location on the
    OnePlus Nord 5.
    
    Values gotten from : # getprop | grep persist.vendor.fingerprint.optical.sensorlocation
    
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- fingerprint/SensorPropsShim.cpp
+++ fingerprint/SensorPropsShim.cpp
@@ -37,20 +37,8 @@ SensorProps SensorPropsInit(SensorProps props) {
             props.sensorType = FingerprintSensorType::HOME_BUTTON;
     }
 
-    auto loc_prop = GetProperty("persist.vendor.fingerprint.optical.sensorlocation", "");
-    if (!loc_prop.empty()) {
-        auto loc = Tokenize(loc_prop, ":");
-        bool loc_parsed = false;
-        if (loc.size() >= 2) {
-            int32_t x, y;
-            loc_parsed = ParseInt(loc[0], &x) && ParseInt(loc[1], &y);
-            if (loc_parsed) {
-                props.sensorLocations[0].sensorLocationX = x;
-                props.sensorLocations[0].sensorLocationY = y;
-            }
-        }
-        LOG_IF(WARNING, !loc_parsed) << "Invalid sensor location input (x::y): " << loc_prop;
-    }
+    props.sensorLocations[0].sensorLocationX = 736;
+    props.sensorLocations[0].sensorLocationY = 2670;
 
     auto size = GetProperty("persist.vendor.fingerprint.optical.iconsize", "");
     if (!size.empty()) {
