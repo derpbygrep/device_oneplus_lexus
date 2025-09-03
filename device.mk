f4f9525c2564d3b95af623f0783fbae1159cc78d
grep <grepfox@tutamail.com>
2025-09-03 22:54:13 +0530

    [tmp] lexus: set `WITH_ADB_INSECURE` as true
    - for debug
    
    Change-Id: I8db0cc06690de69098357048efaa4f78471b7e7f

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
+# ADB
+WITH_ADB_INSECURE := true
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
