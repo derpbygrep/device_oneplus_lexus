37822b6901dd053bb8388a1e8222cd044c9e3cbe
grep <grepfox@tutamail.com>
2025-09-06 13:45:34 +0530

    lexus: Update from CPH2709_15.0.2.501(EX01)
    Change-Id: I44456d80d3f0a9f1ab1daf5a3c6e2ff50ca2a52c

--- lineage_lexus.mk
+++ lineage_lexus.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := CPH2709
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="CPH2709-user 15 UKQ1.231108.001 V.R4T2.36f1d0c-182758a-182e0f3 release-keys" \
-    BuildFingerprint=OnePlus/CPH2709/OP6131L1:15/UKQ1.231108.001/V.R4T2.36f1d0c-182758a-182e0f3:user/release-keys \
+    BuildDesc="qssi_64-user 15 AP3A.240617.008 1754472479511 release-keys" \
+    BuildFingerprint=OnePlus/CPH2709/OP6131L1:15/UKQ1.231108.001/V.R4T2.29e53a9-a9974c-a9a212:user/release-keys \
     DeviceName=OP6131L1 \
     DeviceProduct=CPH2709 \
     SystemDevice=OP6131L1 \
