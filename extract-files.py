8d9c823c582ee0cca5e570b673bdde5548584a0e
grep <grepfox@tutamail.com>
2025-09-15 20:57:30 +0530

    lexus: Update from OnePlus Nord 5 (CPH2709_15.0.2.600(EX01))
    Change-Id: If9f3a387414f6485e578548b6d0d6baf30080346

--- extract-files.py
+++ extract-files.py
@@ -66,7 +66,7 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'odm/etc/camera/CameraHWConfiguration.config': blob_fixup()
-        .regex_replace('SystemCamera =  0;  0;  0;  1;  0; 1;', 'SystemCamera =  0;  0;  0;  0;  0; 0;'),
+        .regex_replace('SystemCamera =  0;  0;  0;  0;  1;', 'SystemCamera =  0;  0;  0;  0;  0;'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V6-ndk.so')
         .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
