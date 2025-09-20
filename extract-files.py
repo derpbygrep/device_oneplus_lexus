95a86ffb4d8f88b1cca170172cccef0699ef134c
grep <grepfox@tutamail.com>
2025-09-20 14:21:04 +0530

    fixup!

--- extract-files.py
+++ extract-files.py
@@ -54,7 +54,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.oplus.hardware.camera_rfi-V1-ndk',
         'vendor.oplus.hardware.cammidasservice-V1-ndk',
         'vendor.oplus.hardware.displaycolorfeature-V1-ndk',
-        'vendor.pixelworks.hardware.display-V2-ndk',
         'vendor.pixelworks.hardware.display@1.0',
         'vendor.pixelworks.hardware.display@1.1',
         'vendor.pixelworks.hardware.display@1.2',
